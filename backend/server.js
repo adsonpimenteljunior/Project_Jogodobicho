0a0c7511107ea27ab677dd3b5c1f262e6bf6de24
Adson Junior <adsonpimenteljunior@gmail.com>
2026-02-22 20:45:20 -0300

    Adicionado banco de dados com Node.js

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Rota simples
+app.get('/api/mensagem', (req, res) => {
+  res.json({ texto: 'Olá do backend Node.js!' });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
